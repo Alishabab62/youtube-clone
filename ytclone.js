d87f68a855b9a4a0388e605f4910ec76268348fd
alishabab62 <alishabab62@gmail.com>
2023-02-13 19:25:59 +0530

    api fixed

--- ytclone.js
+++ ytclone.js
@@ -25,10 +25,8 @@ async function getVideos() {
 
   try {
     let res = await fetch(req);
-    if (!res.ok) {
-      throw new Error(res.statusText);
-    }
     let data = await res.json();
+    console.log(data)
     videoContainer.insertAdjacentHTML('beforeend', data.map((video) => {
 
       return `
